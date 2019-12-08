af1eaf609929e81b2c58695d42c634d6e6b412e4
Marcin Mrugas <marcin.mrugas@snow.dog>
2019-12-08 16:39:18 +0100

    Implemented day 8

--- day_8/src/main/java/pl/mrugacz95/day8/Day8.java
+++ day_8/src/main/java/pl/mrugacz95/day8/Day8.java
@@ -1,7 +1,35 @@
 package pl.mrugacz95.day8;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Day8 {
+    private static final int WIDTH = 25;
+    private static final int HEIGHT = 6;
+
     public static void main(String[] args) {
-        System.out.println("Hello World xd");
+        String input;
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("day_8.in"));
+            input = reader.readLine();
+            reader.close();
+        } catch (IOException e) {
+            System.out.println("Couldn't read input file");
+            return;
+        }
+        long minZeros = Long.MAX_VALUE;
+        long result = -1;
+        for (int layerIdx = 0; layerIdx * WIDTH * HEIGHT < input.length(); layerIdx++) {
+            String layer = input.substring(layerIdx * WIDTH * HEIGHT, (layerIdx + 1) * WIDTH * HEIGHT);
+            long zeros = layer.chars().filter(it -> it == '0').count();
+            if (zeros < minZeros) {
+                minZeros = zeros;
+                long ones = layer.chars().filter(it -> it == '1').count();
+                long twos = layer.chars().filter(it -> it == '2').count();
+                result = ones * twos;
+            }
+        }
+        System.out.println(result);
     }
 }
