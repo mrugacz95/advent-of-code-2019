4432108dfab7f3842bbcadaea4f5bb64ee831d1e
Marcin Mrugas <marcin.mrugas@snow.dog>
2020-03-04 22:22:00 +0100

    Finished day 21 part 1

--- day_21/src/main.rs
+++ day_21/src/main.rs
@@ -4,15 +4,17 @@ use std::collections::{HashMap, VecDeque};
 use std::{char, fs};
 
 fn output(value: isize) {
-    print!("{}", value as u8 as char);
+    match char::from_u32(value as u32) {
+        Some(c) => print!("{}", c),
+        None => println!("{}", value),
+    }
 }
 
 fn main() {
-    let source = fs::read_to_string("main.springscript").expect("Can't read robot source code.");
+    let source = fs::read_to_string("part_1.springscript").expect("Can't read robot source code.");
     let queue: Vec<isize> = source.as_bytes().iter().map(|&x| x as isize).collect();
     let mut queue = VecDeque::from(queue);
 
-    print!("{:?}", queue);
     let memory: HashMap<usize, isize> = fs::read_to_string("day_21.in")
         .expect("Can't open input file")
         .split(",")
