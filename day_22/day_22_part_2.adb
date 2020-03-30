aeb168a1ddcbdf086597bbf2da193f831f970c7d
Marcin Mrugas <marcin.mrugas@snow.dog>
2020-03-30 21:59:32 +0200

    Finished implementation for day 22 part 2

--- day_22/day_22_part_2.adb
+++ day_22/day_22_part_2.adb
@@ -3,13 +3,13 @@ use Ada.Text_IO, Ada.Strings.Fixed;
 
 procedure Day_22_part_2 is
     Input_File : String := "day_22.in";
-    type Long_AS is range -(2 ** 63) .. +(2 ** 63 - 1);
+    type Long_AF is range -(2 ** 63) .. +(2 ** 63 - 1);
     type Operation is record
-        A : Long_AS;
-        B : Long_AS;
+        A : Long_AF;
+        B : Long_AF;
     end record;
-    M : Long_AS := 119315717514047; -- assuming M is prime
-    K : Long_AS := 101741582076661;
+    M : Long_AF := 119315717514047; -- assuming M is prime
+    K : Long_AF := 101741582076661;
     F : Operation;
     type Moves_Array is array (Positive range <>) of Operation;
 
@@ -24,28 +24,28 @@ procedure Day_22_part_2 is
     function Parse_Operation(Line: in String) return Operation is
         Operation_Number : String := "                                  ";
         Operation_String : String := "                                  ";
-        Number : Long_AS := 0;
+        Number : Long_AF := 0;
     begin
         if Line = "deal into new stack" then
-            return (Long_AS(-1), Long_AS(-1));
+            return (Long_AF(-1), Long_AF(-1));
         end if;
         if Line(Line'First .. Line'First + 3) = "cut " then
             Split(Line, 5, Operation_String, Operation_Number);
-            Number := Long_AS'Value(Operation_Number);
-            return (Long_AS(1), -Number);
+            Number := Long_AF'Value(Operation_Number);
+            return (Long_AF(1), -Number);
         end if;
         if Line(Line'First .. Line'First + 19) = "deal with increment " then
             Split(Line, 21, Operation_String, Operation_Number);
-            Number := Long_AS'Value(Operation_Number);
-            return (Number, Long_AS(0));
+            Number := Long_AF'Value(Operation_Number);
+            return (Number, Long_AF(0));
         end if;
         raise Program_Error with "Unrecognized line occurred";
     end Parse_Operation;
 
-    function Mul_Mod(Input_A: Long_AS; Input_B : Long_AS; M: Long_AS) return Long_AS is
-        Y : Long_AS := 0;
-        A : Long_AS := Abs(Input_A);
-        B : Long_AS := Abs(Input_B);
+    function Mul_Mod(Input_A: Long_AF; Input_B : Long_AF; M: Long_AF) return Long_AF is
+        Y : Long_AF := 0;
+        A : Long_AF := Abs(Input_A);
+        B : Long_AF := Abs(Input_B);
         Negative : Boolean := (Input_A >= 0) xor (Input_B >= 0);
     begin
         while B /= 0 loop
@@ -87,10 +87,10 @@ procedure Day_22_part_2 is
        Close (Input);
        return Op;
     end Load_Operation;
-    function Pow_Mod(Input_X: Long_AS; Input_N:Long_AS; M:Long_AS) return Long_AS is
-        Y : Long_AS := 1;
-        N : Long_AS := Input_N;
-        X : Long_AS := Input_X;
+    function Pow_Mod(Input_X: Long_AF; Input_N:Long_AF; M:Long_AF) return Long_AF is
+        Y : Long_AF := 1;
+        N : Long_AF := Input_N;
+        X : Long_AF := Input_X;
     begin
         while N > 0 loop
             if n mod 2 /= 0 then
@@ -101,20 +101,20 @@ procedure Day_22_part_2 is
         end loop;
         return Y;
     end Pow_Mod;
-    function Inverse_Mod(A:Long_AS; M: Long_AS) return Long_AS is
+    function Inverse_Mod(A:Long_AF; M: Long_AF) return Long_AF is
     begin
         return Pow_Mod(A, M - 2, M);
     end Inverse_Mod;
-    function F_Repeated_K_Times_Inverse(F: Operation; K: Long_AS; X: Long_AS) return Long_AS is
-        A : Long_AS := Pow_Mod(F.A, K, M);
-        B : Long_AS := Mul_Mod(Mul_Mod(F.B, (1 - A), M), Inverse_Mod(1 - F.A, M), M);
+    function F_Repeated_K_Times_Inverse(F: Operation; K: Long_AF; X: Long_AF) return Long_AF is
+        A : Long_AF := Pow_Mod(F.A, K, M);
+        B : Long_AF := Mul_Mod(Mul_Mod(F.B, (1 - A), M), Inverse_Mod(1 - F.A, M), M);
     begin
         return Mul_Mod((X - B), Inverse_Mod(A, M), M);
     end F_Repeated_K_Times_Inverse;
 begin
     begin
       F := Load_Operation(Input_File);
-      Put_Line(Long_AS'Image(F_Repeated_K_Times_Inverse(F, K, 2020)));
+      Put_Line(Long_AF'Image(F_Repeated_K_Times_Inverse(F, K, 2020)));
     end;
 end Day_22_part_2;
 
