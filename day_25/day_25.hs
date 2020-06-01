86eaac6554b16c8ea917c66b90b4094c379e33ea
Marcin Mrugas <marcin.mrugas@snow.dog>
2020-06-01 13:17:40 +0200

    Implemented integer computer for day 25

--- day_25/day_25.hs
+++ day_25/day_25.hs
@@ -11,13 +11,18 @@ data Computer = Computer {mem :: Map.Map Integer Integer, pointer :: Integer, re
 read_mem :: Computer -> Integer -> Integer
 read_mem computer pos = Map.findWithDefault 0 pos (mem computer)
 
+shl :: Integer -> Integer -> Integer
+shl num times
+  | times == 0 = num
+  | otherwise = shl (num * 10) (times - 1)
+
 get_mode :: Computer -> Integer -> Integer
-get_mode computer offset = read_mem computer (pointer computer) `div` (floor (10.0 ** realToFrac(offset))) `rem` 10
+get_mode computer offset = read_mem computer (pointer computer) `div` (shl 10 offset)  `rem` 10
 
 get_addr :: Computer -> Integer -> Integer
 get_addr computer offset
-  | mode == 0 = read_mem computer $ read_mem computer (pointer computer + offset)
-  | mode == 1 = read_mem computer (pointer computer + offset)
+  | mode == 0 = read_mem computer (pointer computer + offset)
+  | mode == 1 = pointer computer + offset
   | mode == 2 = read_mem computer (pointer computer + offset) + (relative_base computer)
   where mode = get_mode computer offset
 
@@ -33,58 +38,50 @@ get_param c offset =
 
 user_input :: IO Integer
 user_input = do
+          putStr "Input: "
+          hFlush stdout
           number <- getLine
           return (read number :: Integer)
 
 user_output :: Integer -> IO ()
-user_output value = putStrLn $ show value
+user_output value = putStrLn $ "Output: " ++ show value
 
 instr_cycle :: Computer -> IO Computer
 instr_cycle comp = do
-   putStrLn $ show [(get_mode comp 1), pointer comp]
-   putStrLn $ show comp
    let op = ((read_mem comp (pointer comp)) `rem` 100)
             in case op of
                    1 -> let a = get_param comp 1
                             b = get_param comp 2
-                       in instr_cycle comp { mem = write_mem comp 3 (a+b), pointer = (pointer comp) + 4}
+                        in instr_cycle comp { mem = write_mem comp 3 (a+b), pointer = (pointer comp) + 4}
                    2 -> let a = get_param comp 1
                             b = get_param comp 2
-                       in instr_cycle comp { mem = write_mem comp 3 (a*b), pointer = (pointer comp) + 4}
-
-                   3 ->  let a = get_param comp 1
-                         in do
-                           value <- user_input
-                           instr_cycle comp { mem = write_mem comp a value, pointer = (pointer comp) + 2}
+                        in instr_cycle comp { mem = write_mem comp 3 (a*b), pointer = (pointer comp) + 4}
+                   3 -> do
+                          value <- user_input
+                          instr_cycle comp { mem = write_mem comp 1 value, pointer = (pointer comp) + 2}
                    4 -> let a = get_param comp 1
-                         in do
-                          user_output a
-                          instr_cycle comp {  pointer = (pointer comp) + 2}
+                        in do
+                           user_output a
+                           instr_cycle comp {  pointer = (pointer comp) + 2}
+                   5 -> let a = get_param comp 1
+                            b = get_param comp 2
+                        in instr_cycle comp { pointer = if a /= 0 then b else ((pointer comp) + 3) }
+                   6 -> let a = get_param comp 1
+                            b = get_param comp 2
+                        in instr_cycle comp { pointer = if a == 0 then b else ((pointer comp) + 3) }
+                   7 -> let a = get_param comp 1
+                            b = get_param comp 2
+                        in instr_cycle comp { mem = write_mem comp 3 (if a<b then 1 else 0), pointer = (pointer comp) + 4 }
+                   8 -> let a = get_param comp 1
+                            b = get_param comp 2
+                        in instr_cycle comp { mem = write_mem comp 3 (if a == b then 1 else 0), pointer = (pointer comp) + 4 }
+                   9 -> let a = get_param comp 1
+                        in instr_cycle comp { relative_base = (relative_base comp) + a, pointer = (pointer comp) + 2 }
                    99 -> return comp
-
-
-      -- 4 -> let a = get_param computer 1
-      --      in do
-      --        user_output computer a
-      --        return computer
-      -- 5 -> let a = get_param computer 1
-      --          b = get_param computer 2
-      --      in return computer { pointer = if a /= 0 then b else ((pointer computer) + 3) }
-      -- 6 -> let a = get_param computer 1
-      --          b = get_param computer 2
-      --      in return computer { pointer = if a == 0 then b else ((pointer computer) + 3) }
-      -- 7 -> let a = get_param computer 1
-      --          b = get_param computer 2
-      --      in return computer { mem = write_mem computer 3 (if a<b then 1 else 0 user_input), pointer = (pointer computer) + 4 }
-      -- 8 -> let a = get_param computer 1
-      --          b = get_param computer 2
-      --      in return computer { mem = write_mem computer 3 (if a == b then 1 else 0 user_input), pointer = (pointer computer) + 4 }
-      -- 9 -> let a = get_param computer 1
-      --      in return computer { relative_base = (relative_base computer) + a, pointer = (pointer computer) + 2 }
 
 main = do
     handle <- openFile "day_25.in" ReadMode
     contents <- hGetContents handle
     contents <- instr_cycle ( Computer{ mem = (parse contents), pointer = 0, relative_base = 0})
-    putStrLn $ show contents
+    putStrLn "Done"
     hClose handle
